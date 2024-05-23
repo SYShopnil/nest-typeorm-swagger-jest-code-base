111deecfd2f9b6038e903ca114e69048d76a24e6
SYShopnil <sadmanishopnil@gmail.com>
2024-05-24 03:25:09 +0600

    database connection error fixed

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { DataSource } from 'typeorm';
 
 @Injectable()
-export class AppService {
+export class AppService implements OnModuleInit {
+  constructor(private readonly dataSource: DataSource) {}
+  async onModuleInit() {
+    if (this.dataSource.isInitialized) {
+      console.log('Database connection established successfully!');
+    } else {
+      try {
+        await this.dataSource.initialize();
+        console.log('Database connection established successfully!');
+      } catch (error) {
+        console.error('Failed to establish database connection:', error);
+      }
+    }
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
